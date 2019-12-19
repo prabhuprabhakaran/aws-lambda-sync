88971d39bb94595a8b62d48e47d796814df97fb3
Prabhu Prabhakaran <p.r.c.prabhu@gmail.com>
2019-12-20 00:56:54 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # aws-lambda-sync
-aws-lambda-sync is a utility to upload and download the lambda functions hosted in aws
+
+aws-lambda-sync is a utility to upload and download the lambda functions hosted in AWS. it is user friendly to upload/download the specfic functions or ALL the functions in a stretch.
+
+# Features!
+
+  - While Uploading the changes to AWS, it will do a auto backup the function in AWS to local
+  - While downloding the changes form AWS, it will do a auto backup of the local functions
+  - We can ealily fallback at any point
+  - We can Upload/download functions
+  - All functions are backuped locally
+
+You can also:
+  - Download more than one functions at a time
+  - Upload more than one functions at a time
+  
+## Usage
+aws-lambda-sync is smple to use.
+
+### Prerequiste
+* Install AWS-CLI. Download [Here](https://aws.amazon.com/cli/)
+* Setup your AWS config and IAM User. See [AWS Config](https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-configure.html#cli-quick-configuration), [IAM User](https://console.aws.amazon.com/iam/)
+
+        $ aws configure
+        AWS Access Key ID [None]: ***************
+        AWS Secret Access Key [None]: ********************************
+        Default region name [None]: us-west-2
+        Default output format [None]: json
+
+### Upload Lambda Function
+* To upload ALL Lambda function from Local to AWS
+
+        node awslambdasync.js upload ALL
+
+* To upload specfic Lambda function from Local to AWS
+
+         node awslambdasync.js upload <function_name>
+
+### Download Lambda Function
+* To download ALL Lambda function from Local to AWS
+
+        node awslambdasync.js download ALL
+
+* To download specfic Lambda function from Local to AWS
+
+         node awslambdasync.js download <function_name>
+### Todos
+
+ - Write MORE Tests
+ - Add AWS Lambda Function Configuration Support
+
+License
+----
+MIT
+
+**Free Software, Hell Yeah!**
+
---
+++ awslambdasync.js
@@ -0,0 +1,224 @@
+let download = require("download-file");
+let cmd = require("node-cmd");
+var zipper = require("zip-local");
+let extract = require("extract-zip");
+var fs = require("fs");
+var path = require("path");
+
+let downloadFile = async function(dir, filename, url) {
+  let options = {
+    directory: dir,
+    filename: filename
+  };
+  return new Promise((success, failure) => {
+    download(url, options, function(err) {
+      if (err) {
+        failure(err);
+      } else {
+        success("done");
+      }
+    });
+  });
+};
+
+let extractZip = async function(source, target) {
+  return new Promise((success, failure) => {
+    extract(source, { dir: target }, function(err) {
+      if (err) {
+        failure(err);
+      } else {
+        success("done");
+      }
+    });
+  });
+};
+
+let getAllFunctionList = async function() {
+  return new Promise((success, failure) => {
+    cmd.get("aws lambda list-functions", function(err, data, stderr) {
+      if (err || stderr) {
+        failure(err || stderr);
+      } else {
+        success(data);
+      }
+    });
+  });
+};
+
+let uploadFunction = async function(name, zipFile) {
+  return new Promise((success, failure) => {
+    cmd.get(`aws lambda update-function-code --function-name ${name} --zip-file ${zipFile}`, function(err, data, stderr) {
+      if (err || stderr) {
+        failure(err || stderr);
+      } else {
+        success(data);
+      }
+    });
+  });
+};
+
+let getFunctionDescription = async function(name) {
+  return new Promise((success, failure) => {
+    cmd.get(`aws lambda get-function --function-name ${name}`, function(err, data, stderr) {
+      if (err || stderr) {
+        failure(err || stderr);
+      } else {
+        success(data);
+      }
+    });
+  });
+};
+
+Object.defineProperty(Date.prototype, "YYYYMMDDHHMMSS", {
+  value: function() {
+    function pad2(n) {
+      // always returns a string
+      return (n < 10 ? "0" : "") + n;
+    }
+
+    return this.getFullYear() + pad2(this.getMonth() + 1) + pad2(this.getDate()) + pad2(this.getHours()) + pad2(this.getMinutes()) + pad2(this.getSeconds());
+  }
+});
+
+function sleep(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+}
+
+let init = async function() {
+  try {
+    let isUpload = false;
+    let isALL = false;
+    if (process.argv.length === 4) {
+      if (process.argv[2] === "upload" && process.argv[3] === "ALL") {
+        isALL = true;
+        isUpload = true;
+        console.log("Uploading ALL Functions from ../");
+      } else if (process.argv[2] === "upload" && process.argv[3].startsWith("churchlink_")) {
+        isUpload = true;
+        console.log("Uploading " + process.argv[3] + " Functions from ../");
+      } else if (process.argv[2] === "download" && process.argv[3] === "ALL") {
+        isALL = true;
+        console.log("Downloading ALL Functions to ../");
+      } else if (process.argv[2] === "download" && process.argv[3].startsWith("churchlink_")) {
+        console.log("Downloading " + process.argv[3] + " Functions to ../");
+      } else {
+        console.log("Error in Arguments, Try with churchlink_<Function Name> or ALL");
+        return;
+      }
+    } else {
+      console.log("Error in Arguments, Try with churchlink_<Function Name> or ALL");
+      return;
+    }
+
+    let getFunctionDescriptionResult, downloadFileResult, uploadFunctionResult, extractZipResult;
+    if (!isUpload) {
+      //Download
+      let getAllFunctionListResult = JSON.parse(await getAllFunctionList()).Functions;
+      getAllFunctionListResult.map(async f => {
+        if ((isALL && f.FunctionName.startsWith("churchlink_")) || (!isALL && f.FunctionName === process.argv[3])) {
+          getFunctionDescriptionResult = JSON.parse(await getFunctionDescription(f.FunctionName)).Code.Location;
+
+          var localSourceFolder = `../${f.FunctionName}`;
+          var backupZipFileName = `../.backup/${f.FunctionName}_` + new Date().YYYYMMDDHHMMSS() + "_LOC.zip";
+          var downloadZipFileName = `${f.FunctionName}.zip`;
+
+          //Backup Local Folder if exists
+          if (fs.existsSync(localSourceFolder)) {
+            zipper.sync
+              .zip(localSourceFolder + "/")
+              .compress()
+              .save(backupZipFileName);
+
+            if (fs.existsSync(backupZipFileName)) {
+              console.log("Local Backup Completed for ", f.FunctionName);
+            } else {
+              console.log("Local Backup Failed for ", f.FunctionName);
+              return;
+            }
+          }
+
+          //Delete Zip from Local if exists
+          if (fs.existsSync("../" + downloadZipFileName)) {
+            fs.unlinkSync("../" + downloadZipFileName);
+          }
+
+          //Download from AWS
+          downloadFileResult = await downloadFile("../", downloadZipFileName, getFunctionDescriptionResult);
+          await sleep(500);
+
+          if (fs.existsSync("../" + downloadZipFileName)) {
+            console.log("Download Completed for Function", f.FunctionName);
+            //Delete Local Folder
+            if (fs.existsSync(localSourceFolder)) {
+              fs.rmdirSync(localSourceFolder, { recursive: true });
+            }
+            fs.mkdirSync(localSourceFolder);
+
+            //Extract Zip to Local Folder
+            //zipper.sync.unzip("../" + downloadZipFileName).save(localSourceFolder + "/");
+            extractZipResult = await extractZip("../" + downloadZipFileName, path.resolve(localSourceFolder) + "/");
+
+            //Delete Zip from Local
+            fs.unlinkSync("../" + downloadZipFileName);
+            console.log("Extract Completed for Function", f.FunctionName);
+          } else {
+            console.log("Download Failed for Function", f.FunctionName);
+            return;
+          }
+        }
+      });
+    } else {
+      //Upload
+      let getAllFunctionListResult = JSON.parse(await getAllFunctionList()).Functions;
+      getAllFunctionListResult.map(async f => {
+        if ((isALL && f.FunctionName.startsWith("churchlink_")) || (!isALL && f.FunctionName === process.argv[3])) {
+          getFunctionDescriptionResult = JSON.parse(await getFunctionDescription(f.FunctionName)).Code.Location;
+          var localSourceFolder = `../${f.FunctionName}/`;
+          var uploadZipFileName = `../${f.FunctionName}.zip`;
+
+          //Delete Zip from Local if exists
+          if (fs.existsSync(uploadZipFileName)) {
+            console.log("Old Zip file deleted for ", f.FunctionName);
+            fs.unlinkSync(uploadZipFileName);
+          }
+
+          //Zip Local Folder to upload
+          zipper.sync
+            .zip(localSourceFolder)
+            .compress()
+            .save(uploadZipFileName);
+          console.log("Local Zip creation to upload Completed for ", f.FunctionName);
+
+          //Backup from AWS to Local Folder
+          var backupZipFileName = `${f.FunctionName}_` + new Date().YYYYMMDDHHMMSS() + "_AWS.zip";
+          downloadFileResult = await downloadFile("../.backup/", backupZipFileName, getFunctionDescriptionResult);
+
+          if (fs.existsSync("../.backup/" + backupZipFileName)) {
+            console.log("AWS Backup Completed for ", f.FunctionName);
+          } else {
+            console.log("Backup Zip Not Exists, AWS Backup Failed for ", f.FunctionName);
+            return;
+          }
+
+          if (fs.existsSync(uploadZipFileName)) {
+            //Upload to AWS
+            uploadFunctionResult = await uploadFunction(f.FunctionName, "fileb://" + uploadZipFileName);
+
+            //Delete Local zip Uploaded
+            fs.unlinkSync(uploadZipFileName);
+            console.log("Upload Completed for ", f.FunctionName);
+          } else {
+            console.log("Local Zip Not Exists, Failed to Upload for ", f.FunctionName);
+            return;
+          }
+        }
+      });
+    }
+  } catch (e) {
+    console.log("error", e);
+  }
+};
+
+init();
